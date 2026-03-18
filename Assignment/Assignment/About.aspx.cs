dcc188d95394dbc0f5a05bb53fca2fda171c7853
Sam727518 <leeks050518@gmail.com>
2026-03-19 01:17:36 +0800

    Update About.aspx.cs

--- Assignment/Assignment/About.aspx.cs
+++ Assignment/Assignment/About.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,29 @@ namespace Assignment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                string role = Session["Role"]?.ToString();
+                if (!string.IsNullOrEmpty(role))
+                {
+                    // 用户已登录
+                    hlRegister.Visible = false;
+                    hlLogin.Visible = false;
+                    hlLogout.Visible = true;
 
+                    // 额外处理 Admin/Instructor 导航
+                    if (role == "Admin")
+                    {
+                        hlAdminDashboard.Visible = true;
+                    }
+                }
+                else
+                {
+                    hlRegister.Visible = true;
+                    hlLogin.Visible = true;
+                    hlLogout.Visible = false;
+                }
+            }
         }
     }
 
