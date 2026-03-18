0289a77e090033b3c42f01bd272332766965c720
Sam727518 <leeks050518@gmail.com>
2026-03-19 01:25:17 +0800

    Add files via upload

---
+++ Assignment/Assignment/Logout.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear(); // 清除登录信息
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
